8fd332b4cc5ba81918bfbee6334cac56a6df4eda
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-12-31 20:46:47 +0000

    Teste

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
     print("Consolidacoes do Beecrowd (abelheiro, multidão de abelha, enxame")
 
-if __name__ == "__main__":
-    #main()
-    mediaTwo()
-
 def mediaTwo():
     # problema 1006
     a = input()
@@ -14,3 +10,13 @@ def mediaTwo():
     b = float(b)
     c = float(c)
     print("MEDIA = {:.4f}".format((a*2 + b*3 + c*5)/10))
+
+######## Inicio do programa ########
+
+# Tem que ter declarado antes pra poder chamar / construir.
+
+if __name__ == "__main__":
+    #main()
+    mediaTwo()
+
+###
