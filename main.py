d1047ca07b3d75ae7512171ecb687ceb3b33d3fe
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-12-31 20:39:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    print("Consolidacoes do Beecrowd (abelheiro, multidão de abelha, enxame")
+
+if __name__ == "__main__":
+    #main()
+    mediaTwo()
+
+def mediaTwo
+    # problema 1006
+    a = input()
+    b = input()
+    c = input()
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    print("MEDIA = {:.4f}".format((a*2 + b*3 + c*5)/10))
